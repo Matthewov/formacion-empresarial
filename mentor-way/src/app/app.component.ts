d0d40e60635b32c0ea2f43c96e703f0b31a65199
Julian Sanchez <julian.sanchez@cgiar.org>
2024-11-09 16:43:31 -0500

    updated

--- mentor-way/src/app/app.component.ts
+++ mentor-way/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RegisterComponent } from "./register/register.component";
+import { ReactiveFormsModule } from '@angular/forms';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RegisterComponent, ReactiveFormsModule, NavBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
